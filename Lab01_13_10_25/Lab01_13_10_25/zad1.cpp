0c3525490974d56d4cbece11b397e4f5f1c61745
Patryk Mendelewski <90185123+Illirock@users.noreply.github.com>
2025-10-13 17:16:12 +0200

    Update zad1.cpp

--- Lab01_13_10_25/Lab01_13_10_25/zad1.cpp
+++ Lab01_13_10_25/Lab01_13_10_25/zad1.cpp
@@ -22,9 +22,9 @@ int main()
 	printf("X      X   X   X\n");
 	cout << "\n\n\n\n\n\n\n\n";
 	/*
-	* Zadanie 2
-	*/
-	//cout
+* Zadanie 2
+*/
+//cout
 	cout << "     *     " << endl;
 	cout << "    ***    " << endl;
 	cout << "   *****   " << endl;
@@ -48,4 +48,81 @@ int main()
 	* Zadanie 3
 	*/
 	//cout
+	cout << "Lista Obecności: " << endl;
+	cout << "nr |Imie  |Nazwisko    |" << endl;
+	cout << "------------------------" << endl;
+	cout << "1  |Cypek |Partyka     |" << endl;
+	cout << "2  |Iwo   |Majorczyk   |" << endl;
+	cout << "3  |Mati  |Ligocki     |" << endl;
+	cout << "\n\n\n\n\n\n\n\n";
+	//printf
+	printf("Lista Obecności: \n");
+	printf("nr |Imie  |Nazwisko    |\n");
+	printf("------------------------\n");
+	printf("1  |Cypek |Partyka     |\n");
+	printf("2  |Iwo   |Majorczyk   |\n");
+	printf("3  |Mati  |Ligocki     |\n");
+	printf("\n\n\n\n\n\n\n\n");
+	/*
+	* Zadanie 4
+	*/
+	//cout int
+	/*
+	* 	int a, b;
+		cout << "Podaj 1 liczbe:";
+		cin >> a;
+		cout << "Podaj 2 liczbe:";
+		cin >> b;
+		cout << "Suma: " << a + b << endl;
+		cout << "Iloczyn: " << a * b << endl;
+		cout << "\n\n\n\n\n\n\n\n";
+	*/
+	//cout float
+	/*
+		float a, b;
+		cout << "Podaj 1 liczbe:";
+		cin >> a;
+		cout << "Podaj 2 liczbe:";
+		cin >> b;
+		cout << "Suma: " << a + b << endl;
+		cout << "Iloczyn: " << a * b << endl;
+		cout << "\n\n\n\n\n\n\n\n";
+	*/
+	//printf int
+	/*
+		int a, b;
+		printf("Podaj 1 liczbe:");
+		scanf_s("%d", &a);
+		printf("Podaj 2 liczbe:");
+		scanf_s("%d", &b);
+		printf("Suma: %d\n", a + b);
+		printf("Iloczyn: %d\n", a * b);
+		printf("\n\n\n\n\n\n\n\n");
+	*/
+	//printf float
+	/*
+	 	float a, b;
+		printf("Podaj 1 liczbe:");
+		scanf_s("%f", &a);
+		printf("Podaj 2 liczbe:");
+		scanf_s("%f", &b);
+		printf("Suma: %.2f\n", a + b);
+		printf("Iloczyn: %.2f\n", a * b);
+		printf("\n\n\n\n\n\n\n\n");
+	*/
+	/*
+	* Zadanie 5
+	*/
+	//cout
+	int f = 20;
+	int c = (f - 32) * 5 / 9;
+	cout << f << " stopni F to " << c << " stopni C" << endl;
+	cout << "\n\n\n\n\n\n\n\n";	
+	//printf
+	/*
+	* 	int f = 20;
+		int c = (f - 32) * 5 / 9;
+		printf("%d stopni F to %d stopni C\n", f, c);
+		printf("\n\n\n\n\n\n\n\n");
+	*/
 }
