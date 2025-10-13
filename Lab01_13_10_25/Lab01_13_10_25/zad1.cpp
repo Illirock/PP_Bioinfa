dfeca3e498cb04c3ffa6390cc92e0ec34e85e682
student <student@lab-P6-15.cs.put.poznan.pl>
2025-10-13 16:37:06 +0200

    13.10.2025 1/2 lesson

---
+++ Lab01_13_10_25/Lab01_13_10_25/zad1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*
+	* Zadanie 1
+	*/
+	//cout
+	cout << "XXXX   X       X" << endl;
+	cout << "X  X   XX     XX"<< endl;
+	cout << "XXXX   X X   X X"<< endl;
+	cout << "X      X  X X  X" << endl;
+	cout << "X      X   X   X" << endl;
+	cout << "\n\n\n\n\n\n\n\n";
+	//printf
+	printf("XXXX   X       X\n");
+	printf("X  X   XX     XX\n");
+	printf("XXXX   X X   X X\n");
+	printf("X      X  X X  X\n");
+	printf("X      X   X   X\n");
+	cout << "\n\n\n\n\n\n\n\n";
+	/*
+	* Zadanie 2
+	*/
+	//cout
+	cout << "     *     " << endl;
+	cout << "    ***    " << endl;
+	cout << "   *****   " << endl;
+	cout << "***********" << endl;
+	cout << " ********* " << endl;
+	cout << "  *******  " << endl;
+	cout << " ***   *** " << endl;
+	cout << "**       **" << endl;
+	cout << "\n\n\n\n\n\n\n\n";
+	//printf
+	printf("     *     \n");
+	printf("    ***    \n");
+	printf("   *****   \n");
+	printf("***********\n");
+	printf(" ********* \n");
+	printf("  *******  \n");
+	printf(" ***   *** \n");
+	printf("**       **\n");
+	printf("\n\n\n\n\n\n\n\n");
+	/*
+	* Zadanie 3
+	*/
+	//cout
+}
