e049d731f538f872fa27211dacd447edd81275b2
Illirock <patryk.mendelewski@gmail.com>
2025-12-07 00:40:15 +0100

    Lab07 - Completed

--- Lab07_25_11_25/Lab07_25_11_2025.cpp
+++ Lab07_25_11_25/Lab07_25_11_2025.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -16,51 +17,51 @@ int digit_sum(int n) {
 	for (int i = 0; i <= n; i++) { result += i; }
 	return result;
 }
-
 int digit_sum_recursive(int n)
 {
 	if (n == 0) { return 0; }
 	else { return n + digit_sum_recursive(n - 1); }
 }
 
-string reverse_string(string sentance, int letter_location = 0)  {
-	if (letter_location == sentance.length()) { return ""; }
+string word;
+
+string reverse_string(int letter_location = 0)  {
+	if (letter_location == word.length()) { return ""; }
 	else {
-		return reverse_string(sentance, letter_location+1) + sentance[letter_location];
+		return reverse_string(letter_location+1) + word[letter_location];
 	}
 }
 
-bool is_palindrome(string word, int lenght_checked)
+bool is_palindrome(int lenght_checked)
 {
 	if (lenght_checked == word.length()) { return true; }
 	else
 	{
 		if (word[lenght_checked] == word[word.length() - (1 + lenght_checked)])
 		{
-			return is_palindrome(word, lenght_checked + 1);
+			return is_palindrome(lenght_checked + 1);
 		}
 		else { return false; }
 	}
 }
 
-int max(int array[]) {
-	int max;
-	for (int item = 0; item < sizeof(array); item++) {
-		cout << array[item] << endl;
-		if (item == 0 || max < array[item]) {
-			max = array[item];
+int maxValue(int array[], int size) {
+	int max_val = array[0];
+	for (int i = 1; i < size; i++) {
+		if (array[i] > max_val) {
+			max_val = array[i];
 		}
 	}
-	return max;
+	return max_val;
+}
+int maxValue_recursive(int array[], int size, int index = 0, int current_max = INT_MIN) {
+	if (index == size) { return current_max; }
+	if (array[index] > current_max) {
+		current_max = array[index];
+	}
+	return maxValue_recursive(array, size, index + 1, current_max);
 }
 
-int max_recursive(int array[], int max, int lenght_checked = 0) {
-	if (lenght_checked == sizeof(array)) { return max; }
-	else {
-		if (lenght_checked = 0 || array[lenght_checked] > max) { max = array[lenght_checked]; }
-		return max_recursive(array, max, lenght_checked + 1);
-	}
-}
 
 int Fibbonaci(int n)
 {
@@ -77,7 +78,6 @@ int Power(int base, int exponent)
 	}
 	return result;
 }
-
 int Power_recursive(int base, int exponent)
 {
 	if (exponent == 0) { return 1; }
@@ -99,8 +99,34 @@ int Natural_sum_recurive(int n)
 	return n + Natural_sum_recurive(n - 1);
 }
 
-int main()
+int GCD(int a, int b) {
+	while (b != 0) {
+		int temp = b;
+		b = a % b;
+		a = temp;
+	}
+	return a;
+}
+int GCD_recursive(int a, int b)
 {
+	if (b == 0) { return a; }
+	return GCD_recursive(b, a % b);
+}
+
+int LCM(int a, int b) {
+	return (a * b) / GCD(a, b);
+}
+int LCM_recursive(int a , int b) {
+	int temp = a % b;
+	if (temp == 0) { return a; }
+	return a * LCM_recursive(b, temp) / temp;
+
+}
+
+double executionTime;
+clock_t start, stop;
+
+int main() {
 	// Zadanie 1
 	//int layers;
 	//cout << "How many layers of Christmast Tree you want?" << endl;
@@ -113,19 +139,18 @@ int main()
 	//cout << "Classic: " << digit_sum(5) << endl;
 	//cout << "Recursive: " << digit_sum_recursive(5) << endl;
 	// Zadanie 3
-	// string sentance;
-	// cout << "Enter Phrase" << endl;
-	// cin >> sentance;
-	// cout << "Reversed phrase: " << reverse_string(sentance);
+	// cout << "Enter word" << endl;
+	// cin >> word;
+	// cout << "Reversed word: " << reverse_string();
 	// Zadanie 4
-	int Table[10] = { 0, 9, 8, 43, 56321, 56489, 231, 1, 67423, 3 };
-	cout << "Max value in table is: " << max(Table) << endl;
-	cout << "Max value in table by recursive function is: " << max_recursive(Table, INT_MIN) << endl;
+	// cout << "Enter word" << endl;
+	// cin >> word;
+	// is_palindrome(0) ? cout << "Your word is palidrome" << endl : cout << "Your word isn't palidrome" << endl;
 	// Zadanie 5
-	//string word;
-	//cout << "" << endl;
-	//cin >> word;
-	//is_palindrome(word, 0) ? cout << "Your word is palidrome" << endl : cout << "Your word isn't palidrome" << endl;
+	// int Table[10] = { 0, 9, 8, 43, 56321, 56489, 231, 1, 67423, 3 };
+	// int tableSize = sizeof(Table) / sizeof(Table[0]);
+	// cout << "Max value in table is: " << maxValue(Table, tableSize) << endl;
+	// cout << "Max value in table by recursive function is: "<< maxValue(Table, tableSize) << endl;
 	// Zadanie 6
 	//cout << "Enter nth element of fibbonaci" << endl;
 	//int answear;
@@ -146,6 +171,28 @@ int main()
 	//int answear;
 	//cin >> answear;
 	//cout << "Result: " << Natural_sum_recurive(answear);
-	//Zadanie 10
+	// Zadanie 10
+	// int a, b;
+	// cout << "Enter two numbers to find GCD" << endl;
+	// cin >> a >> b;
+	// cout << "Iterative GCD: " << GCD(a, b) << "\nRecursive GCD: " << GCD_recursive(a, b) << endl;
+	// Zadanie 11
+	// int a, b;
+	// cout << "Enter two numbers to find LCM" << endl;
+	// cin >> a >> b;
+	// if (b > a) { swap(a, b); }
+	// cout << "Iterative LCM: " << LCM(a, b) << "\nRecursive LCM: " << LCM_recursive(a, b) << endl;
+	// Zadanie 12
+	start = clock();
+	digit_sum(4747);
+	stop = clock();
+	executionTime = (double)(stop - start) / CLOCKS_PER_SEC;
+	cout << "Iterative digit sum time: " << executionTime << " seconds" << endl;
+
+	start = clock();
+	digit_sum_recursive(4747);
+	stop = clock();
+	executionTime = (double)(stop - start) / CLOCKS_PER_SEC;
+	cout << "Recursive digit sum time: " << executionTime << " seconds" << endl;
 }
 
