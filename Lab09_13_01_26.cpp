4fe9fff78c5a1577a7e81dbb954b2809510f79f1
Patryk Mendelewski <90185123+Illirock@users.noreply.github.com>
2026-01-20 16:33:07 +0100

    Update and rename LAB09_13_01_26.cpp to Lab09_13_01_26.cpp

--- Lab09_13_01_26.cpp
+++ Lab09_13_01_26.cpp
@@ -25,12 +25,12 @@ int main()
 {
     // Zadanie 1
     //Point punkt;
-    //cout << "WprowadŸ wspó³rzêdne punktu P: " << endl;
+    //cout << "WprowadÅ¸ wspÃ³Â³rzÃªdne punktu P: " << endl;
     //cin >> punkt.x >> punkt.y;
     //cout << "P(" << punkt.x << ", " << punkt.y << ")" << endl;
     // Zadanie 2
     //Rectangle prostokat;
-    //cout << "WprowadŸ wymiary prostok¹ta " << endl;
+    //cout << "WprowadÅ¸ wymiary prostokÂ¹ta " << endl;
     //cin >> prostokat.a >> prostokat.b;
     //cout << "Pole: " << P(prostokat) << "j^2" << endl;
     // Zadanie 3
@@ -38,13 +38,13 @@ int main()
     Triangle* trojakt_2 = new Triangle();
     trojkat_1.a = 0;
     trojkat_1.h = 0;
-    cout << "WprowadŸ d³ugoœæ podstawy oraz wysokoœæ trojk¹ta" << endl;
+    cout << "WprowadÅ¸ dÂ³ugoÅ“Ã¦ podstawy oraz wysokoÅ“Ã¦ trojkÂ¹ta" << endl;
     cin >> (*trojakt_2).a >> (*trojakt_2).h;
-    cout << "Trojk¹t 1: a = " << trojkat_1.a << ", h = " << trojkat_1.h << endl;
-    cout << "Trojk¹t 2: a = " << (*trojakt_2).a << ", h = " << (*trojakt_2).h << endl;
+    cout << "TrojkÂ¹t 1: a = " << trojkat_1.a << ", h = " << trojkat_1.h << endl;
+    cout << "TrojkÂ¹t 2: a = " << (*trojakt_2).a << ", h = " << (*trojakt_2).h << endl;
     transfer(trojakt_2, trojkat_1);
     cout << "================================================" << endl;
-    cout << "Trojk¹t 1: a = " << trojkat_1.a << ", h = " << trojkat_1.h << endl;
-    cout << "Trojk¹t 2: a = " << (*trojakt_2).a << ", h = " << (*trojakt_2).h << endl;
+    cout << "TrojkÂ¹t 1: a = " << trojkat_1.a << ", h = " << trojkat_1.h << endl;
+    cout << "TrojkÂ¹t 2: a = " << (*trojakt_2).a << ", h = " << (*trojakt_2).h << endl;
 }
 
